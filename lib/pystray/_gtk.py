e4f6ec30f4c022536791839bed3f7401c4dc7868
Moses Palmér <mosespalmer@gmail.com>
2016-08-05 11:32:22 +0200

    Added decorator to run function in mainloop

--- lib/pystray/_gtk.py
+++ lib/pystray/_gtk.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import functools
+
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import GLib, GObject, Gtk
@@ -22,19 +24,46 @@ from gi.repository import GLib, GObject, Gtk
 from . import _base
 
 
+def mainloop(f):
+    """Marks a function to be executed in the main loop.
+
+    The function will be sceduled to be executed later in the mainloop.
+
+    :param callable f: The function to execute. Its return value is discarded.
+    """
+    @functools.wraps(f)
+    def inner(*args, **kwargs):
+        def callback(*args, **kwargs):
+            """A callback that executes  ``f`` and then returns ``False``.
+            """
+            try:
+                f(*args, **kwargs)
+            finally:
+                return False
+
+        # Execute the callback as an idle function
+        GObject.idle_add(callback, *args, **kwargs)
+
+    return inner
+
+
 class Icon(_base.Icon):
+    @mainloop
     def _show(self):
         # TODO: Implement
         pass
 
+    @mainloop
     def _hide(self):
         # TODO: Implement
         pass
 
+    @mainloop
     def _update_icon(self):
         # TODO: Implement
         pass
 
+    @mainloop
     def _update_title(self):
         # TODO: Implement
         pass
@@ -43,6 +72,7 @@ class Icon(_base.Icon):
         # TODO: Implement
         pass
 
+    @mainloop
     def _stop(self):
         # TODO: Implement
         pass
