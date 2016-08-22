597aaf9866e5c8737ac893a7eb6804b11f2957ab
Moses Palmér <mosespalmer@gmail.com>
2016-08-22 22:12:49 +0200

    Added generic Pillow image serialiser

--- lib/pystray/_util/__init__.py
+++ lib/pystray/_util/__init__.py
@@ -14,3 +14,36 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import contextlib
+import os
+import tempfile
+
+
+@contextlib.contextmanager
+def serialized_image(image, format, extension=None):
+    """Creates an image file from a :class:`PIL.Image.Image`.
+
+    This function is a context manager that yields a temporary file name. The
+    file is removed when the block is exited.
+
+    :param PIL.Image.Image image: The in-memory image.
+
+    :param str format: The format of the image. This format must be handled by
+        *Pillow*.
+
+    :param extension: The file extension. This defaults to ``format``
+        lowercased.
+    :type extensions: str or None
+    """
+    fd, path = tempfile.mkstemp('.%s' % (extension or format.lower()))
+    try:
+        with os.fdopen(fd, 'wb') as f:
+            image.save(f, format=format)
+        yield path
+
+    finally:
+        try:
+            os.unlink(path)
+        except:
+            raise
