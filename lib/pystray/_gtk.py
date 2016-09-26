1df35557fe8df701a350a134ff9253699e2eb36e
Moses Palmér <mosespalmer@gmail.com>
2016-09-26 15:28:21 +0200

    Use Gtk mainloop from util package

--- lib/pystray/_gtk.py
+++ lib/pystray/_gtk.py
@@ -30,31 +30,9 @@ try:
 except:
     AppIndicator = None
 
+from ._util.gtk import mainloop
 from ._util import serialized_image
 from . import _base
-
-
-def mainloop(f):
-    """Marks a function to be executed in the main loop.
-
-    The function will be sceduled to be executed later in the mainloop.
-
-    :param callable f: The function to execute. Its return value is discarded.
-    """
-    @functools.wraps(f)
-    def inner(*args, **kwargs):
-        def callback(*args, **kwargs):
-            """A callback that executes  ``f`` and then returns ``False``.
-            """
-            try:
-                f(*args, **kwargs)
-            finally:
-                return False
-
-        # Execute the callback as an idle function
-        GObject.idle_add(callback, *args, **kwargs)
-
-    return inner
 
 
 class Icon(_base.Icon):
