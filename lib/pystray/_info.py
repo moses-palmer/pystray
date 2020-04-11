15017bb5885ad6e1f96edb4dc51062389583a932
Moses Palmér <mosespalmer@gmail.com>
2020-04-11 22:49:28 +0200

    Updated copyright

--- lib/pystray/_info.py
+++ lib/pystray/_info.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # pystray
-# Copyright (C) 2016-2017 Moses Palmér
+# Copyright (C) 2016-2020 Moses Palmér
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free
